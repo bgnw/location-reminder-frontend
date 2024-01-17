da48cb6c576b1a7c333ad6c047678a3fbea5e03a
Ben Agnew <ba2005@hw.ac.uk>
2024-01-17 17:53:03 +0000

    Implement drawer navigation layout

--- LocationReminder/app/src/main/java/com/bgnw/locationreminder/MainActivity.kt
+++ LocationReminder/app/src/main/java/com/bgnw/locationreminder/MainActivity.kt
@@ -2,10 +2,30 @@ package com.bgnw.locationreminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_items, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.account -> {
+                Toast.makeText(this, "Account button clicked", Toast.LENGTH_LONG).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
