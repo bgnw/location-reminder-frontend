ad6d1104ad3eb6be369991a161ef294bd886568c
Ben Agnew <ba2005@hw.ac.uk>
2024-02-29 21:18:21 +0000

    Rename Account API structure

--- LocationReminder/app/src/main/java/com/bgnw/locationreminder/api/AccountApi.kt
+++ LocationReminder/app/src/main/java/com/bgnw/locationreminder/api/AccountApi.kt
@@ -1,21 +1,20 @@
-import com.bgnw.locationreminder.api.Account
+import com.bgnw.locationreminder.api.Account_ApiStruct
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
-import retrofit2.http.Url
 
 interface AccountApi {
     @GET("prod-account/{username}")
     fun getAccount(
         @Path("username") username: String?,
         @Query("format") format: String?,
-    ): Call<Account>
+    ): Call<Account_ApiStruct>
 
     @POST("prod-account/create/")
     fun createAccount(
-        @Body body: Account
-    ): Call<Account>
+        @Body body: Account_ApiStruct
+    ): Call<Account_ApiStruct>
 }
