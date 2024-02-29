f1af0cb040d1b67d078681919af0e19c67900cad
Ben Agnew <ba2005@hw.ac.uk>
2024-02-29 21:19:21 +0000

    Implement account log-in (username only)

--- LocationReminder/app/src/main/java/com/bgnw/locationreminder/Account.kt
+++ LocationReminder/app/src/main/java/com/bgnw/locationreminder/Account.kt
@@ -1,32 +1,30 @@
 package com.bgnw.locationreminder
 
+import AccountApi
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.bgnw.locationreminder.api.*
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Account.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Account : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private val viewModel: ApplicationState by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        Requests.initialiseApi()
     }
 
     override fun onCreateView(
@@ -37,23 +35,35 @@ class Account : Fragment() {
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Account.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Account().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    @OptIn(DelicateCoroutinesApi::class)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val devtextview = getView()?.findViewById<TextView>(R.id.account_temp_text)
+        val loginUsername = getView()?.findViewById<EditText>(R.id.account_login_username)
+        val loginPassword = getView()?.findViewById<EditText>(R.id.account_login_password)
+        val loginSubmitButton = getView()?.findViewById<Button>(R.id.account_login_submit)
+
+        viewModel.loggedInUsername.observe(viewLifecycleOwner, Observer {
+                username ->
+            Log.d("OBSERVER", "(in frag) username changed to $username");
+            devtextview?.text = username
+        })
+
+        loginSubmitButton?.setOnClickListener{
+            val username: String = loginUsername?.text.toString()
+            if (username.isEmpty()) { return@setOnClickListener }
+
+            GlobalScope.launch(Dispatchers.Main) {
+                try {
+                    val account = Requests.lookupUser(username, devtextview)
+                    viewModel.loggedInUsername.value = account.username
+                    viewModel.loggedInDisplayName.value = account.display_name // TEMP change to display name
+
+                } catch (e: Exception) {
+                    // Handle exception
+                    Log.e("DJA API", "Error: $e \n${e.printStackTrace()}")
                 }
             }
+        }
     }
 }
