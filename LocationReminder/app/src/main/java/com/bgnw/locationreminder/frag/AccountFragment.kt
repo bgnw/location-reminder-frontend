a16a6e96a41261a9ec92b1bba14afe4da0336dad
Ben Agnew <ba2005@hw.ac.uk>
2024-03-01 15:44:23 +0000

    Improve account page, login/create toggle

--- LocationReminder/app/src/main/java/com/bgnw/locationreminder/frag/AccountFragment.kt
+++ LocationReminder/app/src/main/java/com/bgnw/locationreminder/frag/AccountFragment.kt
@@ -1,5 +1,6 @@
 package com.bgnw.locationreminder.frag
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,7 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -44,6 +47,32 @@ class AccountFragment : Fragment() {
         val loginPassword = getView()?.findViewById<EditText>(R.id.account_login_password)
         val loginSubmitButton = getView()?.findViewById<Button>(R.id.account_login_submit)
 
+        val toggleCreate = getView()?.findViewById<Button>(R.id.account_create_mode_toggle)
+        val toggleLogin = getView()?.findViewById<Button>(R.id.account_login_mode_toggle)
+        val wrapperCreate = getView()?.findViewById<LinearLayout>(R.id.create_account_dialog_wrapper)
+        val wrapperLogin = getView()?.findViewById<LinearLayout>(R.id.login_dialog_wrapper)
+
+        val blue = ContextCompat.getColor(requireContext(), R.color.blue)
+        val grey = ContextCompat.getColor(requireContext(), R.color.grey)
+        toggleCreate?.backgroundTintList = ColorStateList.valueOf(grey)
+        toggleLogin?.backgroundTintList = ColorStateList.valueOf(blue)
+
+        toggleCreate?.setOnClickListener {
+            toggleCreate?.backgroundTintList = ColorStateList.valueOf(blue)
+            toggleLogin?.backgroundTintList = ColorStateList.valueOf(grey)
+            wrapperCreate?.visibility = View.VISIBLE
+            wrapperLogin?.visibility = View.GONE
+        }
+
+        toggleLogin?.setOnClickListener {
+            toggleCreate?.backgroundTintList = ColorStateList.valueOf(grey)
+            toggleLogin?.backgroundTintList = ColorStateList.valueOf(blue)
+            wrapperCreate?.visibility = View.GONE
+            wrapperLogin?.visibility = View.VISIBLE
+        }
+
+
+
         viewModel.loggedInUsername.observe(viewLifecycleOwner, Observer { username ->
             Log.d("OBSERVER", "(in frag) username changed to $username")
             devtextview?.text = username
@@ -68,5 +97,9 @@ class AccountFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private fun showLogin(){
+
     }
 }
