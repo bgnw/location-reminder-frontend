4735d9667503d57dda359b2d742e85a716362f5d
Ben Agnew <ba2005@hw.ac.uk>
2024-03-11 21:02:59 +0000

    Reformat file

--- LocationReminder/app/src/main/java/com/bgnw/locationreminder/NotificationTools.kt
+++ LocationReminder/app/src/main/java/com/bgnw/locationreminder/NotificationTools.kt
@@ -10,32 +10,35 @@ import androidx.core.app.NotificationManagerCompat
 
 class NotificationTools {
 
-    private fun showNotification(
-        context: Context,
-        notifID: Int,
-        title: String,
-        body: String,
-    ) {
-        val builder = NotificationCompat.Builder(context, R.string.channel_id.toString())
-            .setSmallIcon(R.drawable.baseline_info_24).setContentTitle(title)
-            .setContentText(body).setPriority(NotificationCompat.PRIORITY_HIGH)
+    companion object {
+        fun showNotification(
+            context: Context,
+            notifID: Int,
+            title: String,
+            body: String,
+        ) {
+            val builder = NotificationCompat.Builder(context, R.string.channel_id.toString())
+                .setSmallIcon(R.drawable.baseline_info_24).setContentTitle(title)
+                .setContentText(body).setPriority(NotificationCompat.PRIORITY_HIGH)
 
-        with(NotificationManagerCompat.from(context)) {
-            if (ActivityCompat.checkSelfPermission(
-                    context, Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
+            with(NotificationManagerCompat.from(context)) {
+                if (ActivityCompat.checkSelfPermission(
+                        context, Manifest.permission.POST_NOTIFICATIONS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
 
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                notify(notifID, builder.build())
             }
-            notify(notifID, builder.build())
         }
     }
+
 }
