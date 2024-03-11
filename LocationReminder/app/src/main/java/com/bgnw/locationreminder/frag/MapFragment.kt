f14cfaa9439463cae305edbecc02929883266616
Ben Agnew <ba2005@hw.ac.uk>
2024-03-11 21:02:16 +0000

    Add more functions for retrieving nodes from overpass

--- LocationReminder/app/src/main/java/com/bgnw/locationreminder/frag/MapFragment.kt
+++ LocationReminder/app/src/main/java/com/bgnw/locationreminder/frag/MapFragment.kt
@@ -4,13 +4,9 @@ import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -19,8 +15,6 @@ import com.bgnw.locationreminder.MainActivity
 import com.bgnw.locationreminder.R
 import com.bgnw.locationreminder.api.Utils
 import com.bgnw.locationreminder.data.ItemOpportunity
-import com.bgnw.locationreminder.data.TaskItem
-import com.bgnw.locationreminder.data.TaskList
 import com.bgnw.locationreminder.map_aux.MapInfoBox
 import com.bgnw.locationreminder.overpass_api.OverpassElement
 import com.bgnw.locationreminder.overpass_api.OverpassResp
@@ -155,32 +149,61 @@ class MapFragment : Fragment() {
                     for (element: OverpassElement in asianFood.elements) {
                         addMarker(
                             GeoPoint(element.lat, element.lon),
-                            element.tags.name ?: "<name>",
-                            element.tags.amenity ?: "<amenity/shop>"
+                            element.tags?.name ?: "<name>",
+                            element.tags?.amenity ?: "<amenity/shop>"
                         )
                     }
                 }
             }
         }
 
+        fun getPOI(lat: Double, lon: Double, areaRadius: Double) {
+            GlobalScope.launch {
+                val nodes = getAllNearbyPOI(lat, lon, areaRadius)
+                if (nodes != null) {
+                    for (element: OverpassElement in nodes.elements) {
+                        addMarker(
+                            GeoPoint(element.lat, element.lon),
+                            element.tags?.name ?: "<name>",
+                            element.tags?.amenity
+                                ?: element.tags?.shop
+                                ?: element.tags?.place
+                                ?: element.tags?.leisure
+                                ?: element.tags?.office
+                                ?: element.tags?.education
+                                ?: element.tags?.tourism
+                                ?: element.tags?.public_transport
+                                ?: element.tags?.craft
+                                ?: element.tags?.building
+                                ?: element.tags?.man_made
+                                ?: element.tags?.emergency
+                                ?: element.tags?.healthcare
+                                ?: element.tags?.sport
+                                ?: "<amenity/shop>"
+                        )
+                    }
+                }
+            }
+        }
 
         fun updateOnDrag(lat: Double, lon: Double, zoom: Double): Boolean {
-
             val areaRadius = 300+(20-zoom)*80
             Log.d("bgnw_update-map", "updating map due to drag (zoom: $zoom, factor:$areaRadius)")
-
-
-            getAsianFood(lat, lon, areaRadius)
+            // getAsianFood(lat, lon, areaRadius)
+            getPOI(lat, lon, areaRadius)
             return true
         }
 
-
         updateMapButton.setOnClickListener {
-            updateOnDrag(
-                mapView.boundingBox.centerLatitude,
-                mapView.boundingBox.centerLongitude,
-                mapView.zoomLevelDouble
-            )
+            GlobalScope.launch {
+                val resp = getNearbyNodes(55.9086, -3.3187, 400.0, "'amenity'='bicycle_parking'")
+                Log.d("bgnw", resp.toString())
+            }
+//            updateOnDrag(
+//                mapView.boundingBox.centerLatitude,
+//                mapView.boundingBox.centerLongitude,
+//                mapView.zoomLevelDouble
+//            )
         }
 
 
@@ -197,9 +220,6 @@ class MapFragment : Fragment() {
 
         mapView.overlays.add(marker)
     }
-
-
-
 
 
     private suspend fun getNearbyNodes(lat: Double, lon: Double, areaRadius: Double, conditions: String): OverpassResp? {
@@ -220,5 +240,63 @@ class MapFragment : Fragment() {
             return null
         }
     }
+
+    private suspend fun getAllNearbyPOI(lat: Double, lon: Double, areaRadius: Double): OverpassResp? {
+        val overpassQuery = """
+        [out:json][timeout:60];
+        (
+            node(around: $areaRadius, $lat, $lon)["amenity"]
+                ["amenity" != "bench"]
+                ["amenity" != "waste_basket"];
+            node(around: $areaRadius, $lat, $lon)["tourism"];
+            node(around: $areaRadius, $lat, $lon)["shop"];
+            node(around: $areaRadius, $lat, $lon)["leisure"];
+            node(around: $areaRadius, $lat, $lon)["public_transport"];
+            node(around: $areaRadius, $lat, $lon)["craft"];
+            node(around: $areaRadius, $lat, $lon)["office"];
+            node(around: $areaRadius, $lat, $lon)["place"];
+            node(around: $areaRadius, $lat, $lon)["building"];
+            node(around: $areaRadius, $lat, $lon)["man_made"];
+            node(around: $areaRadius, $lat, $lon)["emergency"];
+            node(around: $areaRadius, $lat, $lon)["healthcare"];
+            node(around: $areaRadius, $lat, $lon)["education"];
+            node(around: $areaRadius, $lat, $lon)["sport"];
+            
+            
+        );
+        out geom;
+
+    """.trimIndent()
+
+        /* removed from query for now
+
+            way(around: $areaRadius, $lat, $lon)["amenity"];
+            way(around: $areaRadius, $lat, $lon)["tourism"];
+            way(around: $areaRadius, $lat, $lon)["shop"];
+            way(around: $areaRadius, $lat, $lon)["leisure"];
+            way(around: $areaRadius, $lat, $lon)["public_transport"];
+            way(around: $areaRadius, $lat, $lon)["craft"];
+            way(around: $areaRadius, $lat, $lon)["office"];
+            way(around: $areaRadius, $lat, $lon)["place"]
+                ["place" != "neighbourhood"];
+            way(around: $areaRadius, $lat, $lon)["man_made"];
+            way(around: $areaRadius, $lat, $lon)["emergency"];
+            way(around: $areaRadius, $lat, $lon)["healthcare"];
+            way(around: $areaRadius, $lat, $lon)["education"];
+            way(around: $areaRadius, $lat, $lon)["sport"];
+         */
+
+        Log.d("bgnw_overpass", "QUERY IS: \n$overpassQuery")
+
+
+        try {
+            val response = queryOverpassApi(overpassQuery)
+            return response
+        } catch (e: Exception) {
+            Log.d("bgnw_overpass", "Error: ${e.message}")
+            return null
+        }
+    }
+
 
 }
