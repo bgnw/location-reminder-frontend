9e95f52924d053c804a83d63730397acaee556ee
Ben Agnew <ba2005@hw.ac.uk>
2024-03-11 21:03:10 +0000

    Update tags

--- LocationReminder/app/src/main/java/com/bgnw/locationreminder/overpass_api/OverpassReq.kt
+++ LocationReminder/app/src/main/java/com/bgnw/locationreminder/overpass_api/OverpassReq.kt
@@ -11,13 +11,29 @@ import kotlinx.serialization.json.Json
 
 @Serializable
 data class OverpassElement(val type: String, val id: Long, val lat: Double,
-                           val lon: Double, val tags: Tags)
+                           val lon: Double, val tags: Tags? = null)
 
 @Serializable
 data class Center(val lat: Double, val lon: Double)
 
 @Serializable
-data class Tags(val name: String? = "", val amenity: String? = "", val shop: String? = "")
+data class Tags(
+    val name: String? = "",
+    val amenity: String? = "",
+    val shop: String? = "",
+    val place: String? = "",
+    val leisure: String? = "",
+    val office: String? = "",
+    val education: String? = "",
+    val tourism: String? = "",
+    val public_transport: String? = "",
+    val craft: String? = "",
+    val building: String? = "",
+    val man_made: String? = "",
+    val emergency: String? = "",
+    val healthcare: String? = "",
+    val sport: String? = ""
+)
 
 @Serializable
 data class OverpassResp(val elements: List<OverpassElement>)
